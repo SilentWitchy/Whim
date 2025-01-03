a0f4000dca0c8476288f4b0023bc0e32900e9034
spt11 <amonthofaugust@gmail.com>
2025-01-03 00:55:34 -0600

    Overriding files with updated versions

--- src/main/java/net/august/whim/datagen/ModBlockStateProvider.java
+++ src/main/java/net/august/whim/datagen/ModBlockStateProvider.java
@@ -28,9 +28,6 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-
-
-
         blockWithItem(ModBlocks.BISMUTH_BLOCK);
 
         blockWithItem(ModBlocks.BISMUTH_ORE);
